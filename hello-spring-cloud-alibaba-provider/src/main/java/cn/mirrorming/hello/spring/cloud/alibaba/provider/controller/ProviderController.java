d98424a64f7f7106fe462ec3f4840513466cb281
fh <fh@creditft.com>
2019-11-29 17:57:21 +0800

    配置

--- hello-spring-cloud-alibaba-provider/src/main/java/cn/mirrorming/hello/spring/cloud/alibaba/provider/controller/ProviderController.java
+++ hello-spring-cloud-alibaba-provider/src/main/java/cn/mirrorming/hello/spring/cloud/alibaba/provider/controller/ProviderController.java
@@ -1,6 +1,7 @@
 package cn.mirrorming.hello.spring.cloud.alibaba.provider.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @author mirror
  */
 @RestController
+@RefreshScope
 public class ProviderController {
 
-    @Value("${server.port}")
-    private String port;
+    @Value("${test.config}")
+    private String testConfig;
 
-    @GetMapping(value = "/echo/{message}")
-    public String echo(@PathVariable String message) {
-        return "Hello Nacos Discovery " + message + " , From port :" + port;
+    @GetMapping(value = "/echo")
+    public String echo() {
+        return "Hello Nacos Config :" + testConfig;
     }
 }
